1f0dc9224f95e51855b4d71de533b1af5b6b880d
nic-satoshi <satoshi_kamo@niandc.co.jp>
2024-11-05 15:40:04 +0530

    logical operators

---
+++ Day_2.1_operators-II.py
@@ -0,0 +1,25 @@
+# Equal
+print(5 == 5)
+# Not Equal
+print(5 != 3)
+# Greater Than
+print(5 > 3)
+# Less Than
+print(3 < 5)
+# Greater Than or Equal to
+print(5 >= 5)
+# Less Than or Equal to
+print(3 <= 5)
+
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+print(not True)
+print(not False)
